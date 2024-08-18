fa91a5fc1d04e14766d1ebef5107a3076bceea3e
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2024-08-18 16:30:54 +0200

    feat: login page style

---
+++ app/src/pages/login.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="login-form">
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" />
+        <button>Go</button>
+      </div>
+    </div>
+  );
+}
