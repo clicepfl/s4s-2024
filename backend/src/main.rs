6d1fb920d6b9df410b0f97013fd8760b8ccbceea
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-08-25 14:25:48 +0200

    feat: add fairing for preflight requests

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,46 @@
-use std::sync::Mutex;
-
 use api::State;
-use rocket::launch;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{ContentType, Header, Method, Status};
+use rocket::{catch, catchers, launch};
+use rocket::{Request, Response};
+use std::sync::Mutex;
 
 pub mod api;
 pub mod game;
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+        if request.method() == Method::Options {
+            let body = "";
+            response.set_header(ContentType::Plain);
+            response.set_sized_body(body.len(), std::io::Cursor::new(body));
+            response.set_status(Status::Ok);
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS {})
         .manage(Mutex::<State>::default())
         .mount("/", api::routes())
 }
