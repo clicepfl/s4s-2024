c738a92e5479f13145fb724afea407b0c21d8e38
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2024-08-31 21:12:21 +0200

    feat: update config

--- app/src/api/config.ts
+++ app/src/api/config.ts
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://clic.epfl.ch/s4s-2024/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://clic.epfl.ch/s4s/api";
 export const SESSION_COOKIE_NAME =
-  process.env.NEXT_PUBLIC_SESSION_COOKIE_NAME || "s4s-2024-session";
+  process.env.NEXT_PUBLIC_SESSION_COOKIE_NAME || "s4s-session";
